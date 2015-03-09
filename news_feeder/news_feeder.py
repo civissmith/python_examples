be21880181d861539b9fb5a75bc892485abdb9df
Phil Smith <civis.smith@gmail.com>
2015-03-08 19:43:53 -0600

    Update get_dailymail
    Updated get_dailymail() slurp out articles with descriptions.

--- news_feeder/news_feeder.py
+++ news_feeder/news_feeder.py
@@ -12,10 +12,10 @@
 #
 #
 ################################################################################
+import re
 import urllib.request as ur
 
 def  get_washpost():
-  #page = ur.urlopen('http://www.washingtonpost.com/business/technology')
   page = ur.urlopen('http://www.washingtonpost.com/national')
 
   data = page.readlines()
@@ -36,15 +36,47 @@ def  get_washpost():
 
 def get_reuters():
   # With Reuters, 'topStory' or 'feature' divisions mark articles.
-  #page = ur.urlopen( "http://www.reuters.com/news/science" )
   page = ur.urlopen("http://www.reuters.com/news/us")
   data = page.readlines()
   lines = [ each.decode('utf-8').replace('\r','') for each in data ]
   html = open('page.html', 'w')
   for line in lines:
-    html.write(line)
-
+    if "topStory" in line or "feature" in line:
+      html.write(line)
   html.close()
 
+def  get_dailymail():
+   # Daily Mail: "articletext" denotes article block with description.
+   page = ur.urlopen("http://www.dailymail.co.uk/ushome/index.html")
+   data = page.readlines()
+
+   # Combine every line into a single string.
+   search_string = ""
+   for line in data:
+       search_string += line.decode("utf-8").strip()
+
+   # Search for the articles.
+   links_found = re.findall( r'<div class="articletext">(.*?)</div>', search_string )
+   if links_found:
+       for link in links_found:
+           # Print the link and the description paragraph.
+           # <a href=""/> - link address
+           # <p class="refresh"></p> or <p></p> - description
+           address_found   = re.search(r'<a href="(.*?)">', link)
+           paragraph_found = re.search(r'<p class="refresh">(.*?)</p>|<p>(.*?)</p>', link)
+
+           # Only save links with descriptions.
+           if address_found and paragraph_found:
+
+               address = "http://www.dailymail.co.uk"
+               address += address_found.group(1).replace('" class="js-link-clickable','') 
+               print( address )
+
+               if paragraph_found.group(1):
+                   print( paragraph_found.group(1).replace('<span class="tag-new">NEW</span>','' )) 
+               if paragraph_found.group(2):
+                   print( paragraph_found.group(2).replace('<span class="tag-new">NEW</span>','' )) 
+               print()
+
 if __name__ == "__main__":
-  get_reuters()
+  get_dailymail()
